6e5f2af21ffae48935f3fb706df3cdf6c9cf9e07
SK <shubham.kapoor@csc.fi>
2019-05-19 12:41:07 +0300

    Revert "Merge pull request #1 from juhanikataja/autoscaler"
    This reverts commit c326233e066042179d309c241ab993201b91b26e, reversing
    changes made to 1b2f7b85f6977e04a2117778fa627ca4a850c0ea.
    Reverting chenges back to master

--- load.sh
+++ load.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
-
-if [ -z $1 ]; then
-  echo "Usage: load.sh <url>"
-  exit 1
-fi
-
-# Modify sleeptime for more cpu load
-sleeptime=2.0
-
 for i in {1..5000}
 do
-   curl $1 && echo "" &
-   echo "Curled $1 $i times. Sleeping $sleeptime"
-   sleep $sleeptime
+   echo "Curled $i times"
+   curl $1
 done
--- src/app.py
+++ src/app.py
@@ -1,17 +1,7 @@
 from flask import Flask
-import math
-import time
-
 app = Flask(__name__)
 @app.route("/")
 def home():
-    thesum = 0.0
-    imax = 2000000
-    start = time.time()
-    for i in range(1,imax):
-      thesum = thesum + 1.0/(1.0 + math.exp(i/(1.0*imax)))
-
-    end = time.time()
-    return "Hello, World! The Sum is " + str(thesum) + " in " + str(end-start)
+    return "Hello, World!"
 if __name__ == "__main__":
     app.run(debug=True, port=8080, host='0.0.0.0')
